bb1a9f0e752656eed6a6d1ffd92a9a203d007323
Vijay Selvaraj <VijayAnand_Selvaraj@cable.comcast.com>
2015-09-10 20:00:44 -0400

    RDKB-2588 : WebPA Client review and cleanup Reason for change: Removed CCSP dependencies from webpacore Test Procedure: Test GET,SET and SET-ATOMIC APIs Risks: None Signed-off-by: Vijay Selvaraj <VijayAnand_Selvaraj@cable.comcast.com>
    Change-Id: I485d07f05b60a3adcb0134e8409827760742db4e

---
+++ app/main.c
@@ -0,0 +1,88 @@
+/**
+ * @file main.c
+ *
+ * @description This file defines WebPA's main function
+ *
+ * Copyright (c) 2015  Comcast
+ */
+#include "nopoll.h"
+#include "wal.h"
+#include "websocket_mgr.h"
+#include "stdlib.h"
+#include "signal.h"
+
+/*----------------------------------------------------------------------------*/
+/*                             Function Prototypes                            */
+/*----------------------------------------------------------------------------*/
+static void __terminate_listener(int value);
+static void sig_handler(int sig);
+
+/*----------------------------------------------------------------------------*/
+/*                             External Functions                             */
+/*----------------------------------------------------------------------------*/
+int main()
+{
+	signal(SIGTERM, sig_handler);
+	signal(SIGINT, sig_handler);
+	signal(SIGUSR1, sig_handler);
+	signal(SIGUSR2, sig_handler);
+	signal(SIGSEGV, sig_handler);
+	signal(SIGBUS, sig_handler);
+	signal(SIGKILL, sig_handler);
+	signal(SIGFPE, sig_handler);
+	signal(SIGILL, sig_handler);
+	signal(SIGQUIT, sig_handler);
+	signal(SIGHUP, sig_handler);
+	signal(SIGALRM, sig_handler);
+
+	const char *pComponentName = WEBPA_COMPONENT_NAME;
+	WalInfo("********** Starting component: %s **********\n ", pComponentName); 
+	msgBusInit(pComponentName);
+	createSocketConnection();
+	while(1);
+	return 1;
+}
+
+/*----------------------------------------------------------------------------*/
+/*                             Internal functions                             */
+/*----------------------------------------------------------------------------*/
+static void __terminate_listener(int value) {
+	terminateSocketConnection();
+	return;
+}
+static void sig_handler(int sig)
+{
+	if ( sig == SIGINT ) {
+		signal(SIGINT, sig_handler); /* reset it to this function */
+		WalInfo("WEBPA SIGINT received!\n");
+		//exit(0);
+	}
+	else if ( sig == SIGUSR1 ) {
+		signal(SIGUSR1, sig_handler); /* reset it to this function */
+		WalInfo("WEBPA SIGUSR1 received!\n");
+	}
+	else if ( sig == SIGUSR2 ) {
+		WalInfo("WEBPA SIGUSR2 received!\n");
+	}
+	else if ( sig == SIGCHLD ) {
+		signal(SIGCHLD, sig_handler); /* reset it to this function */
+		WalInfo("WEBPA SIGHLD received!\n");
+	}
+	else if ( sig == SIGPIPE ) {
+		signal(SIGPIPE, sig_handler); /* reset it to this function */
+		WalInfo("WEBPA SIGPIPE received!\n");
+	}
+	else if ( sig == SIGALRM ) {
+		signal(SIGALRM, sig_handler); /* reset it to this function */
+		WalInfo("WEBPA SIGALRM received!\n");
+	}
+	else if( sig == SIGTERM ) {
+		signal(SIGTERM, __terminate_listener);
+		WalInfo("WEBPA SIGTERM received!\n");
+		exit(0);
+	}
+	else {
+		WalInfo("WEBPA Signal %d received!\n", sig);
+		exit(0);
+	}
+}
